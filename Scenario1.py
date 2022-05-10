a7556572e5bb8a090d098aa6d400889cbf6e6828
Grace Brown <graceb340@gmail.com>
2022-05-10 22:14:05 +0200

    begin iteration through episodes, implement action choosing with epsilon greedy algorithm

--- Scenario1.py
+++ Scenario1.py
@@ -1,6 +1,17 @@
 from FourRooms import FourRooms
 import numpy as np
 
+def chooseAction(state, epsilon, Q_vals):
+        # choose action with most expected value
+        action = 0
+        if np.random.uniform(0, 1) <= epsilon:
+            action = np.random.choice(4)
+        else:
+            x = state[0]
+            y = state[1]
+            action = np.argmax(Q_vals[x][y][:])
+        return action
+
 def main():
     aTypes = ['UP', 'DOWN', 'LEFT', 'RIGHT']
     gTypes = ['EMPTY', 'RED', 'GREEN', 'BLUE']
@@ -11,6 +22,10 @@ def main():
     #train
     num_episodes = 100
     fourRoomsObj = FourRooms('simple')
+    for i in range(0, num_episodes):
+        while not fourRoomsObj.isTerminal():
+            state = fourRoomsObj.getPosition()
+            action = chooseAction(state, epsilon, Q_vals)
 
 if __name__ == "__main__":
     main()
